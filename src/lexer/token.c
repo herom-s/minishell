237c8936ca6c3f3ac71bfdae2d9ea4577ea211e0
Thawan Silva <twnpsilva@gmail.com>
2025-11-22 17:39:26 -0300

    fix: refactor functions and rename functions

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -6,30 +6,26 @@
 /*   By: thaperei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 10:54:34 by thaperei          #+#    #+#             */
-/*   Updated: 2025/11/22 15:45:39 by thaperei         ###   ########.fr       */
+/*   Updated: 2025/11/22 17:38:12 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 #include "libft.h"
 
-void	set_operator(t_hash_item operator, char ch, t_token *token, int idx)
+void	check_if_has_duplicates(t_hash_item operator, char ch, t_token *token,
+		int idx)
 {
 	const t_hash_item	dup_operator[] = {{"||", OR_IF}, {"&&", AND_IF},
 	{"<<", DLESS}, {">>", DGREAT}};
 
 	if (ch == *(operator.key))
 	{
-		token->type = dup_operator[idx].value;
-		token->literal = dup_operator[idx].key;
-		token->len = DUP_SIZE;
+		*token = (t_token){dup_operator[idx].value, dup_operator[idx].key,
+			DUP_SIZE};
 	}
 	else
-	{
-		token->type = operator.value;
-		token->literal = operator.key;
-		token->len = CHAR_SIZE;
-	}
+		*token = (t_token){operator.value, operator.key, CHAR_SIZE};
 }
 
 void	check_duplicate_operators(t_lexer *lexer, t_token *token)
@@ -45,7 +41,8 @@ void	check_duplicate_operators(t_lexer *lexer, t_token *token)
 	{
 		if (lexer->ch == *(operators[i].key))
 		{
-			set_operator(operators[i], peek_char(lexer), token, i);
+			check_if_has_duplicates(operators[i], peek_char(lexer),
+				token, i);
 			if (token->len == DUP_SIZE)
 				next_char(lexer);
 		}
@@ -69,9 +66,8 @@ void	check_operators(t_lexer *lexer, t_token *token)
 		{
 			if (lexer->ch == *(operators[i].key))
 			{
-				token->type = operators[i].value;
-				token->literal = operators[i].key;
-				token->len = CHAR_SIZE;
+				*token = (t_token){operators[i].value, operators[i].key,
+					CHAR_SIZE};
 			}
 			i++;
 		}
