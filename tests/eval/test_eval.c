f37399c51e8c25b3e2e9472b8d25401d4edf5313
Thawan Silva <twnpsilva@gmail.com>
2025-12-22 11:21:50 -0300

    fix: access in ast struct

--- tests/eval/test_eval.c
+++ tests/eval/test_eval.c
@@ -6,7 +6,7 @@
 /*   By: hermarti <hermarti@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 12:08:12 by hermarti          #+#    #+#             */
-/*   Updated: 2025/12/04 12:15:42 by hermarti         ###   ########.fr       */
+/*   Updated: 2025/12/22 09:00:49 by thaperei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,10 @@ t_ast	*create_cmd_ast(char *cmd_name, char **args, int arg_count)
 	simple = calloc(1, sizeof(t_ast));
 	if (!simple)
 		return (NULL);
-	simple->type = SIMPLE_CMD;
-	simple->s_simple_cmd.cmd_prefix = NULL;
-	simple->s_simple_cmd.cmd_name = strdup(cmd_name);
-	simple->s_simple_cmd.cmd_suffix = NULL;
+	simple->type = AST_SIMPLE_CMD;
+	simple->u_ast.s_simple_cmd.cmd_prefix = NULL;
+	simple->u_ast.s_simple_cmd.cmd_name = strdup(cmd_name);
+	simple->u_ast.s_simple_cmd.cmd_suffix = NULL;
 	prev_suffix = NULL;
 	i = 0;
 	while (i < arg_count)
@@ -41,14 +41,14 @@ t_ast	*create_cmd_ast(char *cmd_name, char **args, int arg_count)
 		suffix = calloc(1, sizeof(t_ast));
 		if (!suffix)
 			return (NULL);
-		suffix->type = CMD_SUFFIX;
-		suffix->s_cmd_suffix.io_file = NULL;
-		suffix->s_cmd_suffix.word = strdup(args[i]);
-		suffix->s_cmd_suffix.cmd_suffix = NULL;
+		suffix->type = AST_CMD_SUFFIX;
+		suffix->u_ast.s_cmd_suffix.io_file = NULL;
+		suffix->u_ast.s_cmd_suffix.word = strdup(args[i]);
+		suffix->u_ast.s_cmd_suffix.cmd_suffix = NULL;
 		if (prev_suffix)
-			prev_suffix->s_cmd_suffix.cmd_suffix = suffix;
+			prev_suffix->u_ast.s_cmd_suffix.cmd_suffix = suffix;
 		else
-			simple->s_simple_cmd.cmd_suffix = suffix;
+			simple->u_ast.s_simple_cmd.cmd_suffix = suffix;
 		prev_suffix = suffix;
 		i++;
 	}
@@ -62,16 +62,16 @@ void	free_cmd_ast(t_ast *ast)
 
 	if (!ast)
 		return ;
-	if (ast->type == SIMPLE_CMD)
+	if (ast->type == AST_SIMPLE_CMD)
 	{
-		if (ast->s_simple_cmd.cmd_name)
-			free(ast->s_simple_cmd.cmd_name);
-		suf = ast->s_simple_cmd.cmd_suffix;
+		if (ast->u_ast.s_simple_cmd.cmd_name)
+			free(ast->u_ast.s_simple_cmd.cmd_name);
+		suf = ast->u_ast.s_simple_cmd.cmd_suffix;
 		while (suf)
 		{
-			next = suf->s_cmd_suffix.cmd_suffix;
-			if (suf->s_cmd_suffix.word)
-				free((void *)suf->s_cmd_suffix.word);
+			next = suf->u_ast.s_cmd_suffix.cmd_suffix;
+			if (suf->u_ast.s_cmd_suffix.word)
+				free((void *)suf->u_ast.s_cmd_suffix.word);
 			free(suf);
 			suf = next;
 		}
@@ -91,16 +91,16 @@ int	teardown_free_cmd_ast(void **state)
 	if (!state || !*state)
 		return (0);
 	ast = (t_ast *)(*state);
-	if (ast->type == SIMPLE_CMD)
+	if (ast->type == AST_SIMPLE_CMD)
 	{
-		if (ast->s_simple_cmd.cmd_name)
-			free(ast->s_simple_cmd.cmd_name);
-		suf = ast->s_simple_cmd.cmd_suffix;
+		if (ast->u_ast.s_simple_cmd.cmd_name)
+			free(ast->u_ast.s_simple_cmd.cmd_name);
+		suf = ast->u_ast.s_simple_cmd.cmd_suffix;
 		while (suf)
 		{
-			next = suf->s_cmd_suffix.cmd_suffix;
-			if (suf->s_cmd_suffix.word)
-				free((void *)suf->s_cmd_suffix.word);
+			next = suf->u_ast.s_cmd_suffix.cmd_suffix;
+			if (suf->u_ast.s_cmd_suffix.word)
+				free((void *)suf->u_ast.s_cmd_suffix.word);
 			free(suf);
 			suf = next;
 		}
